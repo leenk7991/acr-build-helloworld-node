026b566d52ee4eb11bf27b5e15b51e002f99c98d
Leen Kilani <leen@Leen-Kilani-MBP.local>
2024-09-20 13:46:10 +0300

    test

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const http = require('http')
+const http = require("http");
 
-const port = 80
+const port = 80;
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World\n')
-  response.end('Version: ' + process.env.NODE_VERSION + '\n')
-})
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write("Hello World\n");
+  response.write("updated\n");
+  response.end("Version: " + process.env.NODE_VERSION + "\n");
+});
 
-server.listen(port)
+server.listen(port);
 
-console.log(`Server running at http://localhost: ${port}`)
+console.log(`Server running at http://localhost: ${port}`);
